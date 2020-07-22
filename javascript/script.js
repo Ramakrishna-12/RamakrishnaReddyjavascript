35c8770bc3075b52f27c3801104f7f371d078d72
Ramakrishna-12 <parigaram.ram12@gmail.com>
2020-07-22 16:04:37 +0530

    second commit

--- javascript/script.js
+++ javascript/script.js
@@ -9,37 +9,37 @@
     var pos1=email.search('@');
     if(fname==null || fname==="")
     {
-       document.getElementById('fn').innerHTML="!First name can't be empty";
+       document.getElementById('fnames').innerHTML="!First name can't be empty";
         return false;
     }
     else if(lname==null || lname==="")
     {
-        document.getElementById('ln').innerHTML="!Last name can't be empty";
+        document.getElementById('lnames').innerHTML="!Last name can't be empty";
         return false;
     }
     else if(phone.length!=10)
     {
-        document.getElementById('pn').innerHTML="!Phone number should be exactly 10 digits";
+        document.getElementById('phones').innerHTML="!Phone number should be exactly 10 digits";
         return false;
     }
     else if(email==null || email==="")
     {
-        document.getElementById('em').innerHTML="!Email can't be empty";
+        document.getElementById('emails').innerHTML="!Email can't be empty";
         return false;
     }
     else if(pos==-1)
     {
-        document.getElementById('em').innerHTML='!Email should end with .com';
+        document.getElementById('emails').innerHTML='!Email should end with .com';
         return false;
     }
     else if(pos1==-1)
     {
-        document.getElementById('em').innerHTML="!Email should contain @ symbol";
+        document.getElementById('emails').innerHTML="!Email should contain @ symbol";
         return false;
     }
     else if(password.length<6)
     {
-        document.getElementById('ps').innerHTML="!Password should contain atleast 6 characters";
+        document.getElementById('passwords').innerHTML="!Password should contain atleast 6 characters";
         return false;
     }
     else{
@@ -47,4 +47,13 @@
         return true;
     }
 }
+function removeWarning() {
+    document.getElementById(this.id + "s").innerHTML = "";
+}
+
+document.getElementById("fname").onkeyup = removeWarning;
+document.getElementById("lname").onkeyup = removeWarning;
+document.getElementById("phone").onkeyup = removeWarning;
+document.getElementById("email").onkeyup = removeWarning;
+document.getElementById("password").onkeyup = removeWarning;
     
